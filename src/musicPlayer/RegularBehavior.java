a21b497e1326772dfd21f0535a0fcb0de2ca3f36
goli <shirafkanzeynab63@gmail.com>
2025-04-18 11:47:57 +0330

    add RegularBehavior commands

--- src/musicPlayer/RegularBehavior.java
+++ src/musicPlayer/RegularBehavior.java
@@ -1,4 +1,28 @@
 package musicPlayer;
 
-public class RegularBehavior {
+public class RegularBehavior implements UserBehavior{
+    private int playingLimit;
+
+    public RegularBehavior{
+        this.playingLimit = 5;
+    }
+
+    public void createPlaylist (String Title, User Owner){
+      throw new InvalidOperationException("You are a regular user. regular users cannot create a playlist.");
+    }
+
+    public void playMusic (Music music){
+        if(playingLimit > 0) {
+            music.play();
+            playingLimit--;
+        }
+        if(playingLimit == 0){
+            throw new InvalidOperationException("You can only play 5 songs per day.");
+        }
+
+    }
+
+     private void buyPremium (User owner, int month){
+        owner.behavior = (premiumBehavior) owner.behavior;
+    }
 }
