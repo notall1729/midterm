80e0a9d605644e409336526493fd1b05c3a334db
goli <shirafkanzeynab63@gmail.com>
2025-04-18 11:57:01 +0330

    add PremiumBehavior commands

--- src/musicPlayer/PremiumBehavior.java
+++ src/musicPlayer/PremiumBehavior.java
@@ -1,4 +1,22 @@
 package musicPlayer;
 
-public class PremiumBehavior {
+public class PremiumBehavior implements UserBehavior{
+    private int month;
+
+    public PremiumBehavior(int month){
+       this.month = month;
+    }
+
+    private void createPlaylist (String Title, User Owner){
+        Playlist newPlayList = new PlayList();
+        Owner.playlists.add(newPlayList);
+    }
+
+    public void playMusic (Music music){
+        music.play();
+    }
+
+    private void buyPremium (User owner, int month){
+     this.month += month;
+    }
 }
